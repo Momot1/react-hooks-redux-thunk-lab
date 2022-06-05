f491572771db55fe0e510e74d5b3c1d6f4b42f10
Mark Momot <markmomot03@gmail.com>
2022-06-05 15:17:30 -0400

    commit tree 296673509b53e0a351a7f666f897094a4b77e9e2

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,13 @@
 // write your CatList component here
+
+function CatList({ catPics }) {
+  console.log(catPics);
+
+  const catImages = catPics.map((cat) => (
+    <img alt="cat" src={cat.url} key={cat.id} />
+  ));
+
+  return <>{catImages}</>;
+}
+
+export default CatList;
